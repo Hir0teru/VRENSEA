165eefa1c3c5318336afee79ddf496bae1a9f023
desbeauxrats <deb.brestin@gmail.com>
2018-03-27 17:06:15 +0200

    Avant dernière séance

---
+++ VRTEST/Assets/Musique.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GoogleVR.GVRDemo
+{
+	using UnityEngine;
+
+	[RequireComponent(typeof(Collider))]
+public class Musique : MonoBehaviour {
+
+		private Vector3 startingPosition;
+
+		public Material inactiveMaterial;
+		public Material gazedAtMaterial;
+
+		private bool gazedAt;
+
+		void Start()
+		{
+			startingPosition = transform.localPosition;
+			gazedAt = false;
+			SetGazedAt(false);
+		}
+
+		private void Update()
+		{
+		}
+
+		public void PointerEnter()
+		{
+
+		}
+
+		public void SetGazedAt(bool gazedAt1)
+		{
+			gazedAt = gazedAt1;
+			if (inactiveMaterial != null && gazedAtMaterial != null)
+			{
+				GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+				return;
+			}
+			GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		}
+
+		public void Reset()
+		{
+			transform.localPosition = startingPosition;
+		}
+
+		public void Recenter()
+		{
+			#if !UNITY_EDITOR
+			GvrCardboardHelpers.Recenter();
+			#else
+			if (GvrEditorEmulator.Instance != null)
+			{
+				GvrEditorEmulator.Instance.Recenter();
+			}
+			#endif  // !UNITY_EDITOR
+		}
+	}
+}
+
+
+
+
+
+
+		
+		
+
+		
+		
